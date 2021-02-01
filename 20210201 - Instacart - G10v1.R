238ebbaa5df46ba3658d05e5bd16155e01169b96
KevinDECROCK <77623420+KevinDECROCK@users.noreply.github.com>
2021-02-01 18:52:20 +0100

    Update 20210201 - Instacart - G10v1.R

--- 20210201 - Instacart - G10v1.R
+++ 20210201 - Instacart - G10v1.R
@@ -33,6 +33,9 @@ insta_table$aisle_id <- as.factor(insta_table$aisle_id)
 insta_table$department_id <- as.factor(insta_table$department_id)
 insta_table$reordered <- as.factor(insta_table$reordered)
 
+#insta_table <- insta_table %>% add_column(Prediction=0,.after= "reordered")
+#insta_table$Prediction <- as.factor(insta_table$Prediction)
+
 glimpse(insta_table)
 map(insta_table, class)
 colSums(is.na(insta_table))
@@ -68,5 +71,47 @@ names(joined_prior_train)[names(joined_prior_train) == "Freq.y"] <- "re_order" #
 final_df <- subset(joined_prior_train, select = -c(product_name.y))
 
 final_df
+
+
+
+
+
+#----------------------------------------------------------------------------#
+#--------------------XG BOOST MODEL -----------------------------------------#
+#----------------------------------------------------------------------------#
+
+pacman::p_load("caret","ROCR","lift","xgboost") #Check, and if needed install the necessary packages
+
+#use same training and testing data to ensure like-for-like comparison
+#note - additional memory must be allocated to r before running this code
+
+memory.limit(size=6000000)
+
+training.XG <-model.matrix(reordered~., data = insta_table_prior)
+testing.XG <-model.matrix(reordered~., data = insta_table_train)
+
+model_XGboost<-xgboost(data = data.matrix(training.XG[,-1]), 
+                       label = as.numeric(as.character(insta_table_prior$reordered)), 
+                       eta = 0.1,       # hyperparameter: learning rate 
+                       max_depth = 20,  # hyperparameter: size of a tree in each boosting iteration
+                       nround=50,       # hyperparameter: number of boosting iterations  
+                       objective = "binary:logistic"
+)
+
+XGboost_prediction<-predict(model_XGboost,newdata=testing.XG[,-1], type="response") #Predict classification (for confusion matrix)
+confusionMatrix(as.factor(ifelse(XGboost_prediction>0.22,1,0)),insta_table_prior$reordered,positive="1") #Display confusion matrix
+
+####ROC Curve
+XGboost_pred_testing <- prediction(XGboost_prediction, insta_table_prior$reordered) #Calculate errors
+XGboost_ROC_testing <- performance(XGboost_pred_testing,"tpr","fpr") #Create ROC curve data
+plot(XGboost_ROC_testing) #Plot ROC curve
+
+####AUC
+auc.tmp <- performance(XGboost_pred_testing,"auc") #Create AUC data
+XGboost_auc_testing <- as.numeric(auc.tmp@y.values) #Calculate AUC
+XGboost_auc_testing #Display AUC value: 90+% - excellent, 80-90% - very good, 70-80% - good, 60-70% - so so, below 60% - not much value
+
+#### Lift chart
+plotLift(XGboost_prediction, insta_table_prior$XXXXXXXXXX, cumulative = TRUE, n.buckets = 10) # Plot Lift chart
 
 
