9263235b5b403ab047682d7abe06c1593eaf910b
Robbert-cloud <58041600+Robbert-cloud@users.noreply.github.com>
2021-02-01 17:06:27 +0100

    Update 3 - Ready to transform data

--- 20210201 - Instacart - G10v1.R
+++ 20210201 - Instacart - G10v1.R
@@ -5,7 +5,7 @@
 ##Step 1: IMPORT LIBRARIES
 #-------------------------
 if("pacman" %in% rownames(installed.packages()) == FALSE) {install.packages("pacman")}
-pacman::p_load("caret","ROCR","lift","glmnet","MASS","e1071", "tidyverse", "dplyr", "GGally", "data.table", "readr")
+pacman::p_load("caret","ROCR","lift","glmnet","MASS","e1071", "tidyverse", "dplyr", "GGally", "data.table", "readr", "plyr", "sqldf")
 
 ##Step 2: LOAD THE CSV FILE
 #--------------------------
@@ -37,16 +37,32 @@ glimpse(insta_table)
 map(insta_table, class)
 colSums(is.na(insta_table))
 
-
-##Step 4: FEATURE ENGINEERING
+##Step 4: SPLIT THE DATA
 #-----------------------
+insta_table_prior <- insta_table[insta_table$eval_set == 'prior',] #filter the table to only get the prior orders
+insta_table_train <- insta_table[insta_table$eval_set == 'train',] #filter the table to only get the train orders
 
+nrow(insta_table_prior)
+nrow(insta_table_train)
 
-##Step 5: SPLIT THE DATA
-#-----------------------
+##Step 5: FEATURE ENGINEERING
+#----------------------------
+insta_table_prior_top_1000 <- (head(insta_table_prior, 100))
+View(insta_table_prior_top_1000)
+
+sqldf("SELECT user_id, product_id, product_name, COUNT(*) as Freq
+       FROM insta_table_prior_top_1000
+       GROUP BY user_id, product_id, product_name")
 
 
 ##Step 6: TRANSFORM THE DATA TO A PRODUCT_CUSTOMER PAIR
 #------------------------------------------------------
+
+
+
+
+
+
+
 
 
