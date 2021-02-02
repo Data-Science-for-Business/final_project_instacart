373e913f44e58d64a402e889d4892401f0b3c0df
Robbert-cloud <58041600+Robbert-cloud@users.noreply.github.com>
2021-02-02 09:38:25 +0100

    Update 7: Filter out Test users from prior orders

--- 20210201 - Instacart - G10v1.R
+++ 20210201 - Instacart - G10v1.R
@@ -2,16 +2,19 @@
 ####################INSTACART MARKET BASKET ANALYSIS############################
 #------------------------------------------------------------------------------#
 
-##Step 1: IMPORT LIBRARIES
-#-------------------------
+#-------------------------|
+##Step 1: IMPORT LIBRARIES|
+#-------------------------|
+
 if("pacman" %in% rownames(installed.packages()) == FALSE) {install.packages("pacman")}
 pacman::p_load("caret","ROCR","lift","glmnet","MASS","e1071", "tidyverse", "dplyr", "GGally", "data.table", "readr", "plyr", "sqldf")
 
 memory.limit()
 memory.limit(size=56000)
 
-##Step 2: LOAD AND MERGE THE CSV FILES
-#-------------------------------------
+#-------------------------------------|
+##Step 2: LOAD AND MERGE THE CSV FILES|
+#-------------------------------------|
 #step a): rbind the orders file
 order_products_prior <- fread(file.choose(), showProgress = FALSE)
 order_products_train <- fread(file.choose(), showProgress = FALSE) 
@@ -20,7 +23,7 @@ joined_order_products <- rbind(order_products_prior, order_products_train)
 
 #Step b): left-join the orders to joined_order_products
 order_csv <- fread(file.choose(), showProgress = FALSE)
-expanded_orders <- right_join(order_csv, joined_order_products, by = c("order_id"))
+expanded_orders <- right_join(order_csv, joined_order_products, by = c("order_id")) ##---> here we lose the 75k test orders
 
 nrow(expanded_orders)
 
@@ -46,9 +49,10 @@ str(insta_table)
 nrow(insta_table_test)
 glimpse(insta_table)
 
-##Step 3: TRANSFORM THE DATATYPES
-#-------------------------------
-insta_table$user_id <- as.factor(insta_table$user_id)
+#--------------------------------|
+##Step 3: TRANSFORM THE DATATYPES|
+#--------------------------------|
+#insta_table$user_id <- as.factor(insta_table$user_id)
 insta_table$day_of_the_week <- as.factor(insta_table$order_dow)
 insta_table$order_id <- as.factor(insta_table$order_id)
 insta_table$product_id <- as.factor(insta_table$product_id)
@@ -58,33 +62,41 @@ insta_table$aisle_id <- as.factor(insta_table$aisle_id)
 insta_table$department_id <- as.factor(insta_table$department_id)
 insta_table$reordered <- as.factor(insta_table$reordered)
 
-#insta_table <- insta_table %>% add_column(Prediction=0,.after= "reordered")
-#insta_table$Prediction <- as.factor(insta_table$Prediction)
-
 glimpse(insta_table)
 map(insta_table, class)
 colSums(is.na(insta_table))
 
-##Step 4: SPLIT THE DATA
-#-----------------------
+#-----------------------|
+##Step 4: SPLIT THE DATA|
+#-----------------------|
 insta_table_prior <- insta_table[insta_table$eval_set == 'prior',] #filter the table to only get the prior orders
 insta_table_train <- insta_table[insta_table$eval_set == 'train',] #filter the table to only get the train orders
 
 nrow(insta_table_prior)
-nrow(insta_table_train)
 
-##Step 5: FEATURE ENGINEERING AND TRANSFORM THE DATA TO A PRODUCT_CUSTOMER PAIR
-#------------------------------------------------------------------------------
+#->Create list of 'train' and 'test' user_ids
+train_user_ids <- sqldf("SELECT DISTINCT user_id
+                          FROM insta_table_train")
 
-user_product_prior_df <- sqldf("SELECT user_id, product_id, product_name, COUNT(*) as Freq
-                          FROM insta_table_prior
-                          GROUP BY user_id, product_id, product_name")
+nrow(train_user_ids)
+str(train_user_ids) #-> this is a unique list of user_ids that are part of the training set
+
+#->Select only the insta_table_prior records that belong to eventual train user_ids
+insta_table_prior_trainers_only <- right_join(insta_table_prior, train_user_ids, by = c("user_id")) #--> this join works as a filter
+
+#------------------------------------------------------------------------------|
+##Step 5: FEATURE ENGINEERING AND TRANSFORM THE DATA TO A PRODUCT_CUSTOMER PAIR|
+#------------------------------------------------------------------------------|
+
+user_product_prior_trainer_only_df <- sqldf("SELECT user_id, product_id, product_name, COUNT(*) as Freq
+                                            FROM insta_table_prior_trainers_only
+                                            GROUP BY user_id, product_id, product_name")
 
 user_product_train_df <- sqldf("SELECT user_id, product_id, product_name, COUNT(*) as Freq
                           FROM insta_table_train
                           GROUP BY user_id, product_id, product_name")
 
-joined_prior_train <- left_join(user_product_prior_df, user_product_train_df, by = c("user_id", "product_id"))
+joined_prior_train <- left_join(user_product_prior_trainer_only_df, user_product_train_df, by = c("user_id", "product_id"))
 
 names(joined_prior_train)[names(joined_prior_train) == "Freq.x"] <- "prior_purchase_count" #number of times the product has already been purchased
 joined_prior_train$Freq.y[is.na(joined_prior_train$Freq.y)] <- 0 #Replace NA's with a 0
@@ -95,6 +107,7 @@ final_df$re_order <- as.factor(final_df$re_order)
 str(final_df)
 
 final_df
+gc()
 
 ##Step 6: CLASSIFICATION MODEL 1: XGBOOST
 #----------------------------------------
