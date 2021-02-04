f5b041355d17a47d5f9f41ccc13045934fe27ed1
KevinDECROCK <77623420+KevinDECROCK@users.noreply.github.com>
2021-02-04 22:33:10 +0100

    Create 20210201 - Instacart - G09 - Clustering vf.R

---
+++ 20210201 - Instacart - G09 - Clustering vf.R
@@ -0,0 +1,220 @@
+#clustering
+Sys.setenv(LANG = "en")
+
+#----------------------------------------------------
+#Step 1: create data frame for clustering by user ID
+
+library(data.table)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(psych)
+
+#Function Mode
+Mode2 <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+
+orders <- fread(file.path("C:/Users/Kevin/Documents/GitHub/final_project_instacart/Data", "orders.csv"))
+#orders <- fread(file.choose(), showProgress = FALSE)
+
+orders$user_id <- as.factor(orders$user_id)
+orders$eval_set <- as.factor(orders$eval_set)
+
+df_users <- orders %>%
+  filter(eval_set == "prior") %>%
+  group_by(user_id) %>%
+  #mutate(last = dplyr::last(days_since_prior_order)) %>%
+  summarise(
+    number_of_orders = max(order_number),
+    # number_of_item_in_basket = .... need to add more data we do not have it with only orders
+    order_hour_of_day = mean(order_hour_of_day),
+    order_dow = Mode2(order_dow),
+    days_since_prior_order_average = (sum(days_since_prior_order, na.rm = T))/(max(order_number)-1)
+    #days_since_prior_order_current = last #I have issue with mutate and the grouping, to be solve later or added directly in the featured engineered data
+  )
+
+#head(df_users)
+#View(df_users)
+#nrow(df_users)
+#str(df_users)
+
+#---------------------------------------------------------------
+#Step 2: Preparation
+
+
+# Run below only once, then comment out
+# New versions of the networkD3 package may not work properly, so install the following version
+packageurl <- "https://cran.r-project.org/src/contrib/Archive/networkD3/networkD3_0.2.13.tar.gz"
+install.packages(packageurl, repos=NULL, type="source")
+
+MIN_VALUE = 0.5
+max_data_report = 10
+
+segmentation_attributes_used = c(2,3,4,5) 
+profile_attributes_used = c(2:5) 
+numb_clusters_used = 40 #need to figure out a way to define the number of clusters
+profile_with = "hclust"
+distance_used = "euclidean"
+hclust_method = "ward.D"
+kmeans_method = "Lloyd"
+
+ProjectData <- data.matrix(df_users) 
+str(ProjectData )
+
+segmentation_attributes_used <- intersect(segmentation_attributes_used, 1:ncol(ProjectData))
+profile_attributes_used <- intersect(profile_attributes_used, 1:ncol(ProjectData))
+
+ProjectData_segment <- ProjectData[,segmentation_attributes_used]
+ProjectData_profile <- ProjectData[,profile_attributes_used]
+
+ProjectData_scaled <- apply(ProjectData, 2, function(r) if (sd(r)!=0) (r-mean(r))/sd(r) else 0*r)
+
+
+#------------------------------------------------------------------------------------------------------
+#kmean for user ID
+kmeans_clusters <- kmeans(ProjectData_segment,centers= numb_clusters_used, iter.max=2000, algorithm=kmeans_method)
+
+ProjectData_with_kmeans_membership <- cbind(1:length(kmeans_clusters$cluster),kmeans_clusters$cluster)
+colnames(ProjectData_with_kmeans_membership)<-c("User_id","Cluster_Membership_k")
+
+head(ProjectData_with_kmeans_membership)
+
+user_id_k_means <- (ProjectData_with_kmeans_membership)
+user_id_k_means
+
+user_id_k_means_df <- as.data.frame((user_id_k_means))
+user_id_k_means_df$user_id <- as.factor(user_id_k_means_df$user_id) 
+user_id_k_means_df$Cluster_Membership_k <- as.factor(user_id_k_means_df$Cluster_Membership_k) 
+
+user_id_k_means_df_vF <- user_id_k_means_df
+View(user_id_k_means)
+
+#write.csv(round(ProjectData_with_kmeans_membership, 2), file = "ProjectData_with_kmeans_membership.csv")
+
+
+#---------------------------------
+#clusturing on products
+
+order_product_prior <- fread(file.path("C:/Users/Kevin/Documents/GitHub/final_project_instacart/Data", "order_products__prior.csv"))
+#orders <- fread(file.choose(), showProgress = FALSE)
+
+order_product_prior$product_id <- as.factor(order_product_prior$product_id)
+order_product_prior$order_id <- as.factor(order_product_prior$order_id)
+#str(order_product_prior)
+
+df_products <- order_product_prior %>%
+  group_by(product_id) %>%
+  summarise(
+    number_of_orders = n(),
+    percentage_reordered =  (sum(reordered, na.rm = T)/n()),
+    when_added_to_cart_average = mean(add_to_cart_order),
+    count_added_to_cart_first = sum(add_to_cart_order==1),
+    percentage_added_to_cart_first = count_added_to_cart_first/number_of_orders
+  )
+
+#head(df_products)
+View(df_products)
+
+segmentation_attributes_used_product = c(2,3,4,5,6) 
+profile_attributes_used_product = c(2:6) 
+numb_clusters_used_product = 100
+
+ProjectData_product <- apply(as.matrix(df_products[,1:6]),2,as.numeric) 
+str(ProjectData_product )
+View(ProjectData_product)
+
+segmentation_attributes_used_product <- intersect(segmentation_attributes_used_product, 1:ncol(ProjectData_product))
+profile_attributes_used_product <- intersect(profile_attributes_used_product, 1:ncol(ProjectData_product))
+
+ProjectData_segment_product <- ProjectData_product[,segmentation_attributes_used_product]
+ProjectData_profile_product <- ProjectData_product[,profile_attributes_used_product]
+
+ProjectData_scaled_product <- apply(ProjectData_product, 2, function(r) if (sd(r)!=0) (r-mean(r))/sd(r) else 0*r)
+
+
+#------------------------------------------------------------------------------------------------------
+#kmean on products
+kmeans_clusters_product <- kmeans(ProjectData_segment_product,centers= numb_clusters_used_product, iter.max=2000, algorithm=kmeans_method)
+
+ProjectData_with_kmeans_membership_product <- cbind(1:length(kmeans_clusters_product$cluster),kmeans_clusters_product$cluster)
+colnames(ProjectData_with_kmeans_membership_product)<-c("product_id","product_Cluster_Membership_k")
+
+str(ProjectData_with_kmeans_membership_product)
+
+product_id_k_means <- (ProjectData_with_kmeans_membership_product)
+
+product_id_k_means_df <- as.data.frame((product_id_k_means))
+product_id_k_means_df$product_id <- as.factor(df_products$product_id) 
+product_id_k_means_df$product_Cluster_Membership_k <- as.factor(product_id_k_means_df$product_Cluster_Membership_k) 
+
+
+product_id_k_means_df_vF <- product_id_k_means_df
+View(product_id_k_means_df)
+
+#write.csv(round(ProjectData_with_kmeans_membership_product, 2), file = "ProjectData_with_kmeans_membership_product.csv")
+
+#------------------------------------------------------------------------------------------------------
+#hccluster -> define the number of cluster for users -> answer 4 for ID and 
+
+df_users2 <- filter(df_users,row_number()<15000)
+
+ProjectData_hc <- data.matrix(df_users2) 
+
+segmentation_attributes_used_hc <- intersect(segmentation_attributes_used, 1:ncol(ProjectData_hc))
+profile_attributes_used_hc <- intersect(profile_attributes_used, 1:ncol(ProjectData_hc))
+
+ProjectData_segment_hc <- ProjectData_hc[,segmentation_attributes_used]
+ProjectData_profile_hc <- ProjectData_hc[,profile_attributes_used]
+
+euclidean_pairwise <- as.matrix(dist(head(ProjectData_segment_hc, max_data_report), method="euclidean"))
+euclidean_pairwise <- euclidean_pairwise*lower.tri(euclidean_pairwise) + euclidean_pairwise*diag(euclidean_pairwise) + 10e10*upper.tri(euclidean_pairwise)
+euclidean_pairwise[euclidean_pairwise==10e10] <- NA
+rownames(euclidean_pairwise) <- colnames(euclidean_pairwise) <- sprintf("Obs.%02d", 1:max_data_report)
+
+Pairwise_Distances <- dist(ProjectData_segment_hc, method = distance_used) 
+Hierarchical_Cluster_distances <- dist(ProjectData_segment_hc, method=distance_used)
+Hierarchical_Cluster <- hclust(Hierarchical_Cluster_distances, method=hclust_method)
+
+#plot(Hierarchical_Cluster, labels = NULL, hang = 0.1, 
+#     main = "Cluster dendrogram", sub = NULL,
+#     xlab = NULL, ylab = "Height")
+# -> nothing can be analysed with this
+
+num <- nrow(ProjectData_hc) - 1
+df1 <- cbind(as.data.frame(Hierarchical_Cluster$height[length(Hierarchical_Cluster$height):1]), c(1:num))
+colnames(df1) <- c("distances","index")
+plot(df1, 20, xlab="Number of ", xlim=c(0,20))
+
+#------------------------------------------------------------------------------------------------------
+#hccluster -> define the number of cluster for product -> answer 4 for products
+
+
+df_product2 <- filter(df_products,row_number()<15000)
+#df_product2 <- df_products %>%
+#  slice(1:15000)
+
+ProjectData_hc <- data.matrix(df_product2) 
+
+segmentation_attributes_used_hc <- intersect(segmentation_attributes_used_product, 1:ncol(ProjectData_hc))
+profile_attributes_used_hc <- intersect(profile_attributes_used_product, 1:ncol(ProjectData_hc))
+
+ProjectData_segment_hc <- ProjectData_hc[,segmentation_attributes_used_product]
+ProjectData_profile_hc <- ProjectData_hc[,profile_attributes_used_product]
+
+euclidean_pairwise <- as.matrix(dist(head(ProjectData_segment_hc, max_data_report), method="euclidean"))
+euclidean_pairwise <- euclidean_pairwise*lower.tri(euclidean_pairwise) + euclidean_pairwise*diag(euclidean_pairwise) + 10e10*upper.tri(euclidean_pairwise)
+euclidean_pairwise[euclidean_pairwise==10e10] <- NA
+rownames(euclidean_pairwise) <- colnames(euclidean_pairwise) <- sprintf("Obs.%02d", 1:max_data_report)
+
+Pairwise_Distances <- dist(ProjectData_segment_hc, method = distance_used) 
+Hierarchical_Cluster_distances <- dist(ProjectData_segment_hc, method=distance_used)
+Hierarchical_Cluster <- hclust(Hierarchical_Cluster_distances, method=hclust_method)
+
+num <- nrow(ProjectData_hc) - 1
+df1 <- cbind(as.data.frame(Hierarchical_Cluster$height[length(Hierarchical_Cluster$height):1]), c(1:num))
+colnames(df1) <- c("distances","index")
+plot(df1, 20, xlab="Number of ", xlim=c(0,20))
+
+
